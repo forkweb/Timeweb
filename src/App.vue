99536a92c44e18f38c26a7f8da0e6e744f9ae7e4
fork_web <leshadgrad@mail.ru>
2020-11-05 15:17:39 +0400

    start

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ListPerson msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListPerson from './components/ListPerson.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListPerson
   }
 }
 </script>
---
+++ src/components/ListPerson.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    start
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+const baseUrl = 'https://swapi.dev/api';
+export default {
+  name: 'ListPerson',
+  data() {
+    return {
+      peoples: [],
+    }
+  },
+  computed: {
+    
+  },
+  methods: {
+    GetPoples() {
+      axios.get(`${baseUrl}/people/`).then(response => (this.peoples = response.data.results));
+    },
+  },
+  mounted(){
+    this.GetPoples();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
