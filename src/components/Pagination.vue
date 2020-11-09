2d4d771188b5819d4ac0cd07cdb3ea30cfbfb48f
fork_web <leshadgrad@mail.ru>
2020-11-09 14:31:07 +0400

    pagination_done

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="content peoples">
+  <div class="content pagination">
     
-      <ul>
+      <ul class="pagination__list">
+        <button @click="changePage(currentPage-1)" v-if="hasPrev()">Previous</button>
         <li v-for="p in totalPages" :key="p">
           <button class="pagination-button" :class="{active: currentPage === p}" @click="changePage(p)">{{p}}</button>
         </li>
+        <button @click="changePage(currentPage+1)" v-if="hasNext()">Next</button>
       </ul>
 
   </div>
@@ -21,13 +23,21 @@
     },
     computed: {
       totalPages() {
+
         return Math.ceil(this.total / this.item);
       }
     },
     methods: {
+      hasPrev(){
+        return this.currentPage > 1;
+      },
+      hasNext(){
+        return this.currentPage < this.totalPages;
+      },
       changePage(pageNumber) {
         this.currentPage = pageNumber;
         this.$emit('page-changed', pageNumber);
+        // this.currentPage = this.totalPages;
       }
     }
   }
@@ -43,7 +53,7 @@
   }
 
   .active {
-    background-color: tomato;
+    background-color: rgb(213 38 239 / 31%);
   }
 
 </style>
